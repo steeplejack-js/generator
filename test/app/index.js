d5537bc4b8982e827e63adf4cc12ef0839091828
Simon Emms <simon@simonemms.com>
2017-07-08 22:12:02 +0100

    No runner changes, but made the logging easier to read

--- test/app/index.js
+++ test/app/index.js
@@ -13,122 +13,118 @@ const helpers = require('yeoman-test');
 
 /* Files */
 
-const factory = prompts => helpers
-  .run(path.join(process.cwd(), 'generators', 'app'))
-  .withPrompts(prompts);
+function factory (stackName, prompts) {
+  return () => {
+    log(stackName, `Building new application: ${JSON.stringify(prompts, null, 2)}`);
 
-const runner = (cwd, cmd, timeout = null) => new Promise((resolve, reject) => {
-  const args = cmd.split(' ');
+    return helpers
+      .run(path.join(process.cwd(), 'generators', 'app'))
+      .withPrompts(prompts)
+      /* Run npm install */
+      .then(dir => runner(stackName, dir, 'npm install')
+        .then(dir => runner(stackName, dir, 'npm run ci'))
+        .then(dir => runner(stackName, dir, 'npm run serve', 5000))
+        .then(dir => log(stackName, 'Completed successfully')))
+      .catch(err => log(stackName, err));
+  };
+}
 
-  const command = args.shift();
+function log (stack, message) {
+  console.log(`[${stack}] ${message}`);
+}
 
-  const runningProcess = spawn(command, args, {
-    cwd
-  });
+function runner (stack, cwd, cmd, timeout = null) {
+  return new Promise((resolve, reject) => {
+    log(stack, `Running script: ${cmd}`);
 
-  runningProcess.stdout.on('data', (data) => {
-    console.log(`[${cwd} ${cmd}]: ${data}`);
-  });
+    const args = cmd.split(' ');
 
-  runningProcess.stderr.on('data', (data) => {
-    console.log(`[${cwd} ${cmd}]: ${data}`);
-  });
+    const command = args.shift();
 
-  if (timeout) {
-    setTimeout(() => {
-      runningProcess.kill();
-      resolve();
-    }, timeout);
-  } else {
-    setTimeout(() => {
-      runningProcess.kill();
-      reject(new Error('Exceeded runner timeout'));
-    }, 60000);
+    const runningProcess = spawn(command, args, {
+      cwd
+    });
 
-    runningProcess.on('close', (code) => {
-      if (code === 0) {
+    runningProcess.stdout.on('data', (data) => {
+      log(stack, data);
+    });
+
+    runningProcess.stderr.on('data', (data) => {
+      log(stack, data);
+    });
+
+    if (timeout) {
+      setTimeout(() => {
+        runningProcess.kill();
         resolve();
-        return;
-      }
+      }, timeout);
+    } else {
+      setTimeout(() => {
+        runningProcess.kill();
+        reject(new Error('Exceeded runner timeout'));
+      }, 300000);
 
-      reject();
-    });
-  }
-});
+      runningProcess.on('close', (code) => {
+        if (code === 0) {
+          resolve();
+          return;
+        }
 
-Promise
-  .all([
-    /* Define the applications */
-    factory({
-      author: 'expressNoCompile',
-      compile: false,
-      description: 'expressNoCompile description',
-      envvarPrefix: 'ENC',
-      lint: 'none',
-      name: 'express-no-compile',
-      server: 'express',
-      sockets: false
-    }),
-    factory({
-      author: 'expressCompile',
-      compile: true,
-      description: 'expressCompile description',
-      envvarPrefix: 'EC',
-      lint: 'airbnb',
-      name: 'express-compile',
-      server: 'express',
-      sockets: true
-    }),
-    factory({
-      author: 'restifyNoCompile',
-      compile: false,
-      description: 'restifyNoCompile description',
-      envvarPrefix: 'RNC',
-      lint: 'semistandard',
-      name: 'restify-no-compile',
-      server: 'restify',
-      sockets: false
-    }),
-    factory({
-      author: 'restifyCompile',
-      compile: true,
-      description: 'restifyCompile description',
-      envvarPrefix: 'RC',
-      lint: 'standard',
-      name: 'restify-compile',
-      server: 'restify',
-      sockets: true
-    })
-  ])
-  /* Run npm install */
-  .then(dirs => Promise.all(dirs.map(cwd => new Promise((resolve, reject) => {
-    const install = spawn('npm', [ 'install' ], { cwd });
+        reject();
+      });
+    }
+  });
+}
 
-    install.stdout.on('data', (data) => {
-      console.log(`[${cwd}]: ${data}`);
-    });
+/* Define the applications */
+const apps = [
+  factory('expressNoCompile', {
+    author: 'expressNoCompile',
+    compile: false,
+    description: 'expressNoCompile description',
+    envvarPrefix: 'ENC',
+    lint: 'none',
+    name: 'express-no-compile',
+    server: 'express',
+    sockets: false
+  }),
+  factory('expressCompile', {
+    author: 'expressCompile',
+    compile: true,
+    description: 'expressCompile description',
+    envvarPrefix: 'EC',
+    lint: 'airbnb',
+    name: 'express-compile',
+    server: 'express',
+    sockets: true
+  }),
+  factory('restifyNoCompile', {
+    author: 'restifyNoCompile',
+    compile: false,
+    description: 'restifyNoCompile description',
+    envvarPrefix: 'RNC',
+    lint: 'semistandard',
+    name: 'restify-no-compile',
+    server: 'restify',
+    sockets: false
+  }),
+  factory('restifyCompile', {
+    author: 'restifyCompile',
+    compile: true,
+    description: 'restifyCompile description',
+    envvarPrefix: 'RC',
+    lint: 'standard',
+    name: 'restify-compile',
+    server: 'restify',
+    sockets: true
+  })
+];
 
-    install.stderr.on('data', (data) => {
-      console.log(`[${cwd}]: ${data}`);
-    });
-
-    install.on('close', (code) => {
-      if (code === 0) {
-        resolve(cwd);
-        return;
-      }
-
-      reject(new Error('npm install error'));
-    });
-  }))))
-  .then(dirs => dirs.reduce((thenable, dir) => {
-    return thenable
-      .then(() => runner(dir, 'npm run ci'))
-      .then(() => runner(dir, 'npm run serve', 5000));
-  }, Promise.resolve()))
+apps.reduce((thenable, app) => thenable
+  .then(() => app()), Promise.resolve())
   .then(() => process.exit(0))
-  .catch((err) => {
-    /* Display errors for easier debugging */
-    console.log(err.stack || err);
+  .catch(err => {
+    console.log('GENERAL EXCEPTION');
+    console.log(err);
     process.exit(1);
   });
